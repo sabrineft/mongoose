ad504e1eb7fbfb2212720b65e68c32e24f1b1653
sabrine <sabrinefet@gmail.com>
2022-12-13 14:55:17 +0100

    first commit

---
+++ db_connect.js
@@ -0,0 +1,14 @@
+const mongoos=require("mongoose");
+const { default: mongoose } = require("mongoose");
+
+
+const db_connect=async()=>{
+    try{
+const result= await mongoose.connect(process.env.DB_URI);
+console.log("database is connected")
+    }catch(error){
+        console.log(error)
+
+    }
+}
+module.exports=db_connect;
